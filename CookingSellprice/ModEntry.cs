332c0834189be531abb89aff45fbc73c8bf25033
Plastikmensch <Plastikmensch@users.noreply.github.com>
2023-08-06 15:48:36 +0200

    Update to SMAPI 3.18 (#1)
    * Update to SMAPI 3.18
    
    Essentially a rewrite for future proofing by removing deprecated code.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    
    * Add prices of new items
    
    Add prices of new 1.5 items.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    
    * Clarify ingredient quality having no effect
    
    To avoid confusion, clarify how recipes work.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    
    * Minor refactor
    
    Adds new helper function for debug logging.
    
    Changed some variable names and code format for better readability.
    
    The logic remained unchanged.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    
    * Bump version to 2.0.0
    
    This feels more like a major than minor change.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- CookingSellprice/ModEntry.cs
+++ CookingSellprice/ModEntry.cs
@@ -2,79 +2,100 @@
 using StardewModdingAPI;
 using StardewValley;
 using System.Collections.Generic;
+using StardewModdingAPI.Events;
 
 namespace CookingSellprice
 {
-    public class ModEntry : Mod, IAssetEditor
+    public class ModEntry : Mod
     {
-        // Should be a ModConfig option
+        //TODO: Should be a ModConfig option
         private bool debug = false;
 
-        public bool CanEdit<T>(IAssetInfo asset)
+        public void OnAssetRequested(object sender, AssetRequestedEventArgs e)
         {
-            if (asset.AssetNameEquals("Data/ObjectInformation"))
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/ObjectInformation"))
             {
-                return true;
+                e.Edit(asset =>
+                {
+                    IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
+
+                    // Iterate through cooking recipes
+                    foreach (KeyValuePair<string, string> pair in CraftingRecipe.cookingRecipes)
+                    {
+                        string[] recipe = pair.Value.Split('/');
+                        DebugLog("recipe " + string.Join(",", recipe));
+                        string[] ingredients = recipe[0].Split(' ');
+                        // Some mods specify amount, even if it defaults to 1,
+                        // to avoid bugs, amount gets omitted
+                        int cookedItemId = int.Parse(recipe[2].Split(' ')[0]);
+                        // the base value of cooked items
+                        int price = 50;
+
+                        // Iterate over ingredients
+                        for (int i = 0; i < ingredients.Length; i += 2)
+                        {
+                            int ingredientId = int.Parse(ingredients[i]);
+                            int ingredientAmount = int.Parse(ingredients[i + 1]);
+
+                            switch (ingredientId)
+                            {
+                                // Fish Category
+                                case -4:
+                                    price += 100 * ingredientAmount;
+                                    break;
+                                // EggEggEgg
+                                case -5:
+                                    price += 50 * ingredientAmount;
+                                    break;
+                                // Milk Category
+                                case -6:
+                                    price += 125 * ingredientAmount;
+                                    break;
+                                default:
+                                    string[] ingredientData = data[ingredientId].Split('/');
+                                    price += int.Parse(ingredientData[1]) * ingredientAmount;
+                                    break;
+                            }
+                        }
+
+                        DebugLog("recipe[2] " + cookedItemId);
+                        // Get cooked item data
+                        string[] fields = data[cookedItemId].Split('/');
+                        string oldPrice = fields[1];
+
+                        // Don't decrease price
+                        if (int.Parse(oldPrice) < price)
+                        {
+                            fields[1] = price.ToString();
+                            string newinfo = string.Join("/", fields);
+                            data[cookedItemId] = newinfo;
+                            DebugLog($"new price for {pair.Key}: {price}. Was: {oldPrice}");
+                        }
+                        else
+                        {
+                            DebugLog($"price for {pair.Key}: {oldPrice}");
+                        }
+
+                        // Used to create the table in the Readme. Convenient.
+                        //Monitor.Log($"{pair.Key} | {fields[1]}g");
+                    }
+                });
             }
-            return false;
         }
 
-        public void Edit<T>(IAssetData asset)
+        // Used to log verbose information
+        // Less spammy, since verbose logging in SMAPI will print verbose log of every installed mod
+        private void DebugLog(string message)
         {
-            if (asset.AssetNameEquals("Data/ObjectInformation"))
+            if (debug)
             {
-                IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
-                //iterate through cooking recipes
-                foreach (KeyValuePair<string, string> pair in CraftingRecipe.cookingRecipes)
-                {
-                    string[] recipe = pair.Value.Split('/');
-                    if(debug) Monitor.Log("recipe " + string.Join(",", recipe));
-                    string[] ingredients = recipe[0].Split(' ');
-                    // Some mods specify amount, even if it defaults to 1,
-                    // to avoid bugs, amount gets omitted
-                    string yield = recipe[2].Split(' ')[0];
-                    // the base value of cooked items
-                    int price = 50;
-                    // iterate over ingredients
-                    for (int i = 0; i < ingredients.Length; i += 2)
-                    {
-                        switch (int.Parse(ingredients[i]))
-                        {
-                            //Fish Category
-                            case -4:
-                                price += 100 * int.Parse(ingredients[i + 1]);
-                                break;
-                            //EggEggEgg
-                            case -5:
-                                price += 50 * int.Parse(ingredients[i + 1]);
-                                break;
-                            //Milk Category
-                            case -6:
-                                price += 125 * int.Parse(ingredients[i + 1]);
-                                break;
-                            default:
-                                string[] information = data[int.Parse(ingredients[i])].Split('/');
-                                price += int.Parse(information[1]) * int.Parse(ingredients[i + 1]);
-                                break;
-                        }
-                    }
-                    if (debug) Monitor.Log("recipe[2] " + yield);
-                    string[] fields = data[int.Parse(yield)].Split('/');
-                    //Don't decrease price
-                    if (int.Parse(fields[1]) < price)
-                    {
-                        fields[1] = price.ToString();
-                        string newinfo = string.Join("/", fields);
-                        data[int.Parse(yield)] = newinfo;
-                    }
-                    //Monitor.VerboseLog($"new price for {pair.Key}: {price}");
-                }
+                Monitor.Log(message);
             }
         }
 
         public override void Entry(IModHelper helper)
         {
-
+            helper.Events.Content.AssetRequested += this.OnAssetRequested;
         }
     }
 }
